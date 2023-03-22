0bc214dfc3df83f9e6d0bdc2f1a863719b63fc64
jans <jans@EMPOWER-JAN>
2023-03-22 14:15:52 +0100

    Add project files.

---
+++ AutomationScript_ClassLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationScript_ClassLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("AutomationScript_ClassLibrary")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D456DA60-49CB-437A-91A7-64EFCA07C407")]
+[assembly: AssemblyVersion("1.0.0.0")]
